dc4b92b69ad1928d4adf803bd663bbfbdd56e215
Wagner Inaba <wagner.inaba@gmail.com>
2019-11-18 00:42:00 -0300

    Update test4.py
    Limpa comentarios

--- test4.py
+++ test4.py
@@ -10,21 +10,6 @@ def create_task():
     arquivo.write(request.data.decode("utf-8") + '\n')
     arquivo.close()
     return 'Sucesso!\n\n'
-     
-#    if request.headers['Content-Type']=='application/json':
-#        return 'Content-Type: '+ str(request.headers['Content-Type'])   
-#    else:
-#        return request.data    
-
-#    task = {
-#        'id': tasks[-1]['id'] + 1,
-#        'title': request.json['title'],
-#        'description': request.json.get('description', ""),
-#        'done': False
-#    }
-#    tasks.append(task)
-
-#    return jsonify({'task': request.json}), 201
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='5000', debug=True)
